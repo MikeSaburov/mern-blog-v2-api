cc5e1b82f505cb0358caad4dbb9c1be9b070db3a
Mikhail Saburov <saburov1985@gmail.com>
2023-12-18 22:02:20 +1100

    feature(index): получаем и сохраняем изображение в uploads

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 const User = require('./models/User');
 
@@ -72,6 +73,13 @@ app.post('/logout', async (req, res) => {
   res.cookie('token', '').json('Вы вышли.До встречи!');
 });
 
-app.post('/create', uploadMiddleware.single('file'), async (req, res) => {});
+app.post('/create', uploadMiddleware.single('file'), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path + '.' + ext;
+  fs.renameSync(path, newPath);
+  res.json({ ext });
+});
 
 app.listen(4000);
